8e17b7fb045ec7f279404b38a36ead7228e86e6c
Matthijs van Niekerk <matthijs.vanniekerk@shapeq.io>
2022-12-09 12:01:00 +0100

    juice up the example

--- src/validate.ts
+++ src/validate.ts
@@ -1,15 +1,15 @@
-import Ajv from "ajv";
 import rfqEvent from "../schema/rfq-event.json";
 import example from "../data/example.json";
 import {RfqEvent} from "./types";
+import Ajv from "ajv";
 import addFormats from "ajv-formats";
 
 const ajv = new Ajv();
 addFormats(ajv);
 const validator = ajv.compile<RfqEvent>(rfqEvent);
 
-if (validator(example)) {
-    console.log("Validated!");
-} else {
-    console.log(validator.errors);
+if (!validator(example)) {
+    throw new Error(`Validation failed:\n${JSON.stringify(validator.errors, null, 2)}`);
 }
+const validated: RfqEvent = example;
+console.log("Validated!");
